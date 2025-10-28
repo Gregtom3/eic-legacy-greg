dccb6baa819e8470d0720eafa74422832cf22f91
Gregtom3 <gamatousek@gmail.com>
2025-10-28 16:26:31 -0400

    Add completion messages and script instructions to dataset creation scripts

--- analysis/filippo/create_dataset.rb
+++ analysis/filippo/create_dataset.rb
@@ -3,3 +3,7 @@
 Dir.chdir("submodules/epic-analysis") do
   system("ruby hpc/run-local-slurm-pipeline.rb --runcard ../../analysis/filippo/runcards/ep_piplus.yaml")
 end
+
+puts "=========================== Pipeline completed ==========================="
+puts "Run the following scripts **OUTSIDE** the eic-shell environment:"
+puts "  bash /w/hallb-scshelf2102/clas12/users/gmat/eic/eic-legacy-greg/submodules/epic-analysis/hpc/project_scripts/run-PYTHIA8.ep_piplus.sh"
--- analysis/yorgo/create_dataset.rb
+++ analysis/yorgo/create_dataset.rb
@@ -4,3 +4,8 @@ Dir.chdir("submodules/epic-analysis") do
   system("ruby hpc/run-local-slurm-pipeline.rb --runcard ../../analysis/yorgo/runcards/eHe3_10x166_pipluspiminus.yaml")
   system("ruby hpc/run-local-slurm-pipeline.rb --runcard ../../analysis/yorgo/runcards/ep_10x100_pipluspiminus.yaml")
 end
+
+puts "=========================== Pipeline completed ==========================="
+puts "Run the following scripts **OUTSIDE** the eic-shell environment:"
+puts "  bash /w/hallb-scshelf2102/clas12/users/gmat/eic/eic-legacy-greg/submodules/epic-analysis/hpc/project_scripts/run-PYTHIA8.ep_pipluspiminus.sh"
+puts "  bash /w/hallb-scshelf2102/clas12/users/gmat/eic/eic-legacy-greg/submodules/epic-analysis/hpc/project_scripts/run-BeAGLE.eHe3_pipluspiminus.sh"
